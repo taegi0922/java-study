c375ef86f85f37553bf5f6334271dc60fc1daee4
Taegi <rlaxorl0298@naver.com>
2024-11-18 14:27:16 +0900

    커밋2

--- FirstProject/src/First_Java.java
+++ FirstProject/src/First_Java.java
@@ -5,6 +5,10 @@ public class First_Java {
 	public static void main(String[] args) {
 		System.out.println("hello java");
 		System.out.println("김태기");
+		System.out.println("김태기");
+		System.out.println("김태기");
+		System.out.println("김태기");
+		
 		
 	}
 
