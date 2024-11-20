e2d20be0cd64b11f3fd1b8e1624dced18afc5c62
Taegi <rlaxorl0298@naver.com>
2024-11-20 10:23:07 +0900

    11.20 단항연산자2

---
+++ Chapter01_자바소개/src/연산자_단항연산자2.java
@@ -0,0 +1,25 @@
+/*
+ * 		부정연산자 : ! 
+ * 			=> 조건문 
+ * 				=> 로그인이 안된 경우 
+ * 				=> 예약날짜가 아닌 경우...
+ * 				=> 게임 : 턴
+ * 			=> 무조건 boolean 만 사용이 가능 
+ * 				boolean bCheck = false;
+ * 				!bCheak = true;
+ * 
+ * */
+public class 연산자_단항연산자2 {
+	public static void main(String[] args) {
+		boolean bCheck = false;
+		System.out.println(bCheck); //false
+		bCheck = !bCheck;
+		System.out.println(bCheck); //true 
+		
+		int kor =55;
+		if(!(kor>=60)) {
+		
+		}
+	}
+
+}
