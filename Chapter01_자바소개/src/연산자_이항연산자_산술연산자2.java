7f11e3065ec0ea2c37c6634426eef01e26cb48da
Taegi <rlaxorl0298@naver.com>
2024-11-20 12:02:32 +0900

    11.20  Math.random()

---
+++ Chapter01_자바소개/src/연산자_이항연산자_산술연산자2.java
@@ -0,0 +1,15 @@
+/*
+ * => 임의로 변수 초기관 
+ * => 난수 = >>Math.random()*100
+ * 1~100 (int)(Math.random()*100)+1
+ * */
+public class 연산자_이항연산자_산술연산자2 {
+	public static void main(String[] args) {
+		int a =(int)(Math.random()*100)+1;
+		System.out.println(a);
+		int b =(int)(Math.random()*100)+1;
+		System.out.println(b);
+		System.out.println(a+b);
+	}
+
+}
