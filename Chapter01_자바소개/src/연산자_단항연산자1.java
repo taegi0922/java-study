56af3e0e37c290ee6ff4ba8003f3476894b5c664
Taegi <rlaxorl0298@naver.com>
2024-11-20 10:05:50 +0900

    11.20 단항연산자

---
+++ Chapter01_자바소개/src/연산자_단항연산자1.java
@@ -0,0 +1,19 @@
+/*
+ * 	1.단항 연산자
+ * 	  증감 연산자(++,--) : 1증가,1감소 
+ * 		=>반복문에서 주로 사용
+ * 		=>게임쪽에서 주로 사용 
+ * 		=>전치 연산자 / 후치 연산자  
+ * 	
+ * 	기능 : 출력 / 증가 
+ * 			int a =10;
+ * 			System.out.println(a++);
+ * */
+public class 연산자_단항연산자1 {
+	public static void main(String[] args) {
+		int a =10;
+		System.out.println(a++);
+		
+	}
+
+}
