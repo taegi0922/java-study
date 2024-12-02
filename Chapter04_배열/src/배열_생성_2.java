e019b73813d4241cab8420ccf68f076debdd5f1f
Taegi <rlaxorl0298@naver.com>
2024-12-02 10:05:13 +0900

    12.02 배열

--- Chapter04_배열/src/배열_생성_2.java
+++ Chapter04_배열/src/배열_생성_2.java
@@ -7,13 +7,15 @@ public class 배열_생성_2 {
 	
 		for(int i=0;i<arr.length;i++) {
 			 arr[i] =(int)(Math.random()*10)+1; 
+			 
 			 for (int j = 0; j < i; j++) {
 				if(arr[i] == arr[j]) {
 					i--; 
-					break;
+					break; //break를 건 이유는 같은거 만나면 
+						   //뒤에꺼까지 안보고 바로 빠져나가게 걸어준다
 				}
-				
 			}
+			 
 		}
 		for (int i : arr) {
 			System.out.println(i+" ");
