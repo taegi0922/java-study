ad885216cb877438cadd7f012109aa78fc8a69cb
Taegi <rlaxorl0298@naver.com>
2024-12-18 14:50:40 +0900

    12.18 지니뮤직 데이터 가져와서 윈도우 띄우기

--- Chapter08_예외처리_2/src/com/sist/music/MusicVO.java
+++ Chapter08_예외처리_2/src/com/sist/music/MusicVO.java
@@ -2,32 +2,12 @@ package com.sist.music;
 
 import lombok.Data;
 
-//캡슐화 => 데이터 보호(데이터를 모아서 전송할 목적)
+// 캡슐화 => 데이터 보호 (데이터를 모아서 전송할 목적)
+
 @Data
 public class MusicVO {
-	private String title;
-	private String single;
-	private String album;
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getSingle() {
-		return single;
-	}
-	public void setSingle(String single) {
-		this.single = single;
-	}
-	public String getAlbum() {
-		return album;
-	}
-	public void setAlbum(String album) {
-		this.album = album;
-	}
-	
-	
-	
-	// 사영자 연결 => getter /setter
+   private String title;
+   private String singer;
+   private String album;
+   // 사용자 연결 => getter/setter 
 }
