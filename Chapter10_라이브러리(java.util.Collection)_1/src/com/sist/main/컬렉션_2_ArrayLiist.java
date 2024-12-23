af06898a3866a6a39d356ff4f2fef559c009e838
Taegi <rlaxorl0298@naver.com>
2024-12-23 10:38:12 +0900

    12.23 lombok.EqualsAndHashCode 재정의

--- Chapter10_라이브러리(java.util.Collection)_1/src/com/sist/main/컬렉션_2_ArrayLiist.java
+++ Chapter10_라이브러리(java.util.Collection)_1/src/com/sist/main/컬렉션_2_ArrayLiist.java
@@ -1,14 +1,39 @@
 package com.sist.main;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.*;
+//@EqualsAndHashCode
+//@Data
+@Getter
+@Setter
 class Sawon{
 	private int sabun;
 	private String name;
 	private String dept;
+	
+	public Sawon(int sabun, String name, String dept) {
+		this.sabun = sabun;
+		this.name = name;
+		this.dept = dept;
+	}
+	
+	
 }
 public class 컬렉션_2_ArrayLiist {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Sawon s1 = new Sawon(1,"홍길동","개발부");
+		Sawon s2 = new Sawon(1,"홍길동","개발부");
+		
+		if(s1.equals(s2)) { //객체 비교
+			System.out.println("같은 사원입니다");
+		}else {
+			System.out.println("다른 사원");
+		}
 	}
 
 }
